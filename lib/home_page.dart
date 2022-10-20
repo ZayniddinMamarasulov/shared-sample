1be43e574d3ff8f6ea4d22e33d27133aca564d78
Zayniddin <tuit0778@gmail.com>
2022-10-20 22:30:56 +0500

    changes

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shared_sample/local_data/storage_repository.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,50 +12,42 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   int _count = 0;
 
-  SharedPreferences? _pref;
-
-  Future<int> getCount() async {
-    _pref = await SharedPreferences.getInstance();
-    _count = _pref?.getInt("count") ?? 0;
+  int getCount() {
+    _count = StorageRepository.getInt("count");
     return _count;
   }
 
   void increment() async {
-    _pref = await SharedPreferences.getInstance();
-    _pref?.setInt("count", _count);
+    await StorageRepository.saveInt("count", _count);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getCount();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Shared sample"),
-      ),
-      body: FutureBuilder<int>(
-        future: getCount(),
-        builder: (context, AsyncSnapshot<int> snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            var count = snapshot.data;
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(),
-                Text(count.toString(), style: TextStyle(fontSize: 32)),
-                TextButton(
-                    onPressed: () {
-                      setState(() {
-                        _count++;
-                        increment();
-                      });
-                    },
-                    child: Text("Add"))
-              ],
-            );
-          }
-          return Container();
-        },
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Shared sample"),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(),
+            Text(_count.toString(), style: TextStyle(fontSize: 32)),
+            TextButton(
+                onPressed: () {
+                  setState(() {
+                    _count++;
+                    increment();
+                  });
+                },
+                child: Text("Add"))
+          ],
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:shared_sample/home_page.dart';
+import 'package:shared_sample/local_data/storage_repository.dart';
 import 'package:shared_sample/login_page.dart';
 import 'package:shared_sample/splash_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  StorageRepository.getInstance();
   runApp(const MyApp());
 }
 
